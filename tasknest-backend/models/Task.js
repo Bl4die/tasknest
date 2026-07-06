4b2501f9ef7353adf8a3ea563afac84d942afb92
Bl4die <tharindugenshin@gmail.com>
2026-07-06 06:53:26 +0530

    linking the task to user

--- tasknest-backend/models/Task.js
+++ tasknest-backend/models/Task.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const taskSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String },
-    status: { type: String, default: 'Pending' }
+    status: { type: String, default: 'Pending' },
+    // Linking the task to a user
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'WorkspaceAccount', required: true }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Task', taskSchema);
